66e9b66a23a8728dda25b97f9848860b88f2bd01
Pusit.S <pusitclb@gmail.com>
2025-02-02 02:53:15 +0700

    Task 11: javascript object

---
+++ json.js
@@ -0,0 +1,10 @@
+const object = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+};
+
+console.log(object);
+console.log(object.name);
+const jsonStr = JSON.stringify(object);
+console.log(jsonStr);
