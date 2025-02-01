6798fbf0e2d044d8bdef02c1bfcfd48861c42bd7
Pusit.S <pusitclb@gmail.com>
2025-02-01 21:07:15 +0700

    Task 7: for loop name

--- for.js
+++ for.js
@@ -1,5 +1,8 @@
-// NOTE: ทำไมสามารถ i = color = list ได้?
-let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
-for (let i = 0; i < color.length; i++) {
-    console.log(color[i]);
+let name = ["Alice", "Bob", "Charlie", "David", "Eve"]
+for (let i = 0; i < name.length; i++) {
+    if (name[i].length > 6) {
+        console.log('สวัสดีคุณ ' + name[i]);
+    } else {
+        console.log('Hello ' + name[i]);
+    }
 }
